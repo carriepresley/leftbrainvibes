bb1a58692b32edd4db49bd3e3ab7f2f28679634c
carriepresley <carriepresley15@gmail.com>
2020-12-15 15:29:09 -0600

    css is life

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,17 +6,33 @@ import gif from "../assets/gif.gif";
 
 function HomePage(props) {
     return (
-        <div className= "home-hero">
-         <img id ='home-gif' src={gif} alt = "left brain vibes pic"></img>
-        <p id ='home-p-text'>
-            We keep the left brain vibes chill by bringing <font color = '#ff6633'>creativity</font> to <font color ='#cc9966'>analytical</font>, <font color = '#ff6699'>mathematical</font> learning.
-        </p>
         
-        <div className= 'd-flex justify-content-center'>
+<div>
+    <div className= 'row'>
+        <div className = 'col-md-12'>
+        <p  className ='home-p'>
+             We keep the left brain vibes chill by bringing  <font color = '#ff6633'>creativity</font> to <font color ='#cc9966'>analytical</font>, <font color = '#ff6699'>mathematical</font> learning.
+        </p>
+        </div>
+        <div className ='row'>
+        <div className = 'col-md-6'>
+            <a href="/courses" className ="button2">Let's Start!</a>
+            </div>
+
+           
+            <div className = 'col-md-6'>
+                <img src={gif} alt = "left brain vibes pic" className = 'home-gif text-right' ></img>
+            </div>
+
+            
+            
+            
+
+        </div>
+                 
+        </div>
+</div>
        
-        <a href="/courses" id ="button2">Let's Start!</a>
-        </div>
-        </div>
     );
 }
 
