195e6e437ed7c2709de2a0c6c384b05d7ee7e9ef
carriepresley <carriepresley15@gmail.com>
2020-12-21 11:46:39 -0600

    another bootstrap format update

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -3,6 +3,10 @@ import emailjs from "emailjs-com";
 
 function ContactForm(){
 
+    function submitFunction(){
+        document.getElementById("submitbutton").style.backgroundColor = '#ff6699';
+        document.getElementById("submitbutton").value = "Message Sent";
+    }
 
     function sendEmail(e) {
         e.preventDefault();
@@ -22,7 +26,7 @@ function ContactForm(){
     return (
         <div>
             <div className ='container'>
-                <form onSubmit ={sendEmail} >
+                <form onSubmit ={sendEmail}>
                 <div className='row pt-5 mx-auto'>
                     <div className='col-8 form-group mx-auto'>
                         <input type='text' className='form-control' placeholder='Name' name ='name'/>
@@ -37,7 +41,7 @@ function ContactForm(){
                         <textarea className='form-control' id = "" cols = '33' rows = '7' placeholder='Message' name ='message'></textarea>
                     </div>
                     <div className ='col-8 pt-3 mx-auto'>
-                        <input type='submit' className= 'btn btn-info' value='Send Message'></input>
+                        <input type='submit' className= 'btn btn-info' value='Send Message' id ='submitbutton' onClick = {submitFunction}></input>
                     </div>
                 </div>
                 </form>
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -14,13 +14,13 @@ function HomePage(props) {
 <div>
     
     <div className= 'row text-left'>
-        <div className = 'col-lg-12'>
+        <div className = 'col-xl-12'>
         <p  className ='home-p'>
              We keep the left brain vibes chill with a splash of <font color = '#ff6633'>creativity</font> woven into <font color ='#cc9966'>analytical</font>, <font color = '#ff6699'>mathematical</font> learning.
         </p>
         </div>
         <div className ='row'>
-        <div className = 'col-lg-6'>
+        <div className = 'col-xl-6'>
         
             <Link to='/courses' className ="button2">Let's Start!</Link>
             <Router>
@@ -30,7 +30,7 @@ function HomePage(props) {
             </div>
 
            
-            <div className = 'col-md-6'>
+            <div className = 'col-xl-6'>
                 <img src={gif} alt = "left brain vibes pic" className = 'home-gif text-right' ></img>
             </div>
 
